dc8cc31035f965a07b799661eec867cc2747f567
jorge <jorge-mor@outlook.es>
2023-01-03 00:49:02 +0100

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: "development",
     entry: './src/Index.tsx',
     output: {
-        path: path.resolve(__dirname, './dist'),
+        path: path.resolve(__dirname, './public'),
         filename: 'bundle.js'
     },
     devServer: {
