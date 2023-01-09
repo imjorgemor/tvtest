6857338a1977e1df94a82f02b1439087c9a34e7f
jorge <jorge-mor@outlook.es>
2023-01-09 11:35:19 +0100

    Add readme and update bitmovin key

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './index.css';
 import './styles/styles.scss';
-//import fonts to avoid view default browser font
 import './assets/fonts/RakutenSans.woff2';
 import './assets/fonts/RakutenSansBd.woff2';
 import './assets/fonts/RakutenSansBlk.woff2';
