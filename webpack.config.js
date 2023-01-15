c097eb81f90d83bfc7e197d3099f2b4c7ea23c21
jorge <jorge-mor@outlook.es>
2023-01-15 18:12:04 +0100

    Update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,113 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
-const Dotenv = require('dotenv-webpack');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ReactRefreshTypeScript = require('react-refresh-typescript');
 
-module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, "./src/index.tsx"),
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'index_bundle.js',
-        publicPath: '/',
-        sourceMapFilename: "[name].js.map"
-    },
-    devtool: 'inline-source-map',
-    plugins: [new ReactRefreshWebpackPlugin()].filter(Boolean),
-    devServer: {
-        host: 'localhost.rakuten.tv',
-        port: "3000",
-        historyApiFallback: true,
-        static: path.resolve(__dirname, './dist'),
-        open: true,
-        hot: true,
-        liveReload: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|ts)x?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = (env, argv) => {
+
+    return {
+        mode: isDevelopment ? 'development' : 'production',
+        output: {
+            filename: !isDevelopment
+                ? '[name].[contenthash].js'
+                : 'main.js',
+            path: path.resolve(__dirname, 'build'),
+            assetModuleFilename: "assets/[hash][ext][query]"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    include: path.join(__dirname, 'src'),
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                //pollyfill required for supporting navigators => takes more time to compile
+                                // [
+                                //     '@babel/preset-env', { targets: "defaults", "debug": true, "useBuiltIns": "usage", "corejs": 3 }
+                                // ],
+                                [
+                                    '@babel/preset-react', { runtime: "automatic" } //not necessary import react
+                                ]
+                            ]
+                        }
+                    }
                 },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                },
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        "style-loader",
+                        // Translates CSS into CommonJS
+                        "css-loader",
+                        // Compiles Sass to CSS
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
+                },
+                {
+                    test: /\.tsx?$/,
+                    include: path.join(__dirname, 'src'),
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                configFile: isDevelopment ? 'tsconfig.dev.json' : 'tsconfig.json',
+                                transpileOnly: true,
+                                ...(isDevelopment && {
+                                    getCustomTransformers: () => ({
+                                        before: [ReactRefreshTypeScript()],
+                                    }),
+                                }),
+                            },
+                        },
+                    ],
+                },
+            ]
+        },
+        plugins: [
+            isDevelopment && new ReactRefreshWebpackPlugin(),
+            new ForkTsCheckerWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+                favicon: "./public/favicon.ico",
+            }),
+        ].filter(Boolean),
+        devServer: {
+            open: true, // open navigator
+            port: 3000,
+            compress: true,
+            hot: true,
+            host: 'localhost.rakuten.tv',
+            //routing config
+            historyApiFallback: true,
+            // eslint-disable-next-line no-dupe-keys
+            historyApiFallback: {
+                disableDotRule: true
             },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    "style-loader",
-                    // Translates CSS into CommonJS
-                    "css-loader",
-                    // Compiles Sass to CSS
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
-        ],
-    },
-    ignoreWarnings: [/"Devtools failed to parse source map"/],
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js'],
-    },
-    // eslint-disable-next-line no-dupe-keys
-    plugins: [
-        new Dotenv(),
-    ]
+            client: { overlay: true }, //errors are shown in the web
+        },
+        devtool: isDevelopment ? 'source-map' : 'inline-source-map',//view source as original files (takes more time to compile)
+        ignoreWarnings: [/"Devtools failed to parse source map"/],
+        resolve: {
+            extensions: ['.tsx', '.ts', '.jsx', '.js'],  //accept both extensions on src files
+        },
+    };
 };
